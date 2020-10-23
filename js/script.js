eb57a5068ec3811566adf19f94181c11845e56bf
Steph Barker <sbarkerdrums@gmail.com>
2020-10-23 16:53:41 -0700

    adds skill to the list

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// IPO - Input Process Output
+
+// Constants and Variables (State)
+let skills;
+
+
+
+// Cached Element References
+const $button = $('button');
+const $ul = $('ul');
+const $input = $('input');
+
+// Event Listeners
+$button.on('click', handleAddSkill)
+$ul.on('click', handleDelete)
+
+// Functions
+
+init();
+
+function init () {
+    skills = [];
+    render();
+}
+
+function handleAddSkill() {
+    // store the value from the input tag inside a local variable
+    const skill = $input.val();
+    // check to make sure we have skill data
+    if(skill) {
+        //create the UI for a skill
+        const $skill = $(`<li><span>X</span>${skill}</li>`)
+        // push skill UI into the skills list
+        skills.push($skill);
+        // clear the input tag's value
+        $input.val('');
+        // call render() 
+        render()
+    } else return;
+}
+
+function handleDelete() {
+    alert('elements inside of ul tag clicked')
+}
+
+function render() {
+//to take the list of skills in the skills array and add them to the UL tag
+if(!skills.length) $ul.css('margin-bottom', '30px');
+else $ul.css('margin-bottom', '0px');
+$ul.html(skills);
+}
